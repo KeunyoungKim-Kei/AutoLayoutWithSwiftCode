d78dda356cf606b6b188112b38156b24c7401367
Keun young Kim <app@meetkei.com>
2015-01-29 13:11:10 +0900

    commit sample

--- AutoLayoutWithSwiftCode/ViewController.swift
+++ AutoLayoutWithSwiftCode/ViewController.swift
@@ -12,14 +12,88 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let centerLabel = UILabel()
+        centerLabel.backgroundColor = UIColor.yellowColor()
+        centerLabel.text = "Hello Auto Layout"
+        centerLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        var centerXConstraint = NSLayoutConstraint(item: centerLabel,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: .CenterX,
+            multiplier: 1.0,
+            constant: 0.0)
+        let centerYConstraint = NSLayoutConstraint(item: self.view,
+            attribute: .CenterY,
+            relatedBy: .Equal,
+            toItem: centerLabel,
+            attribute: .CenterY,
+            multiplier: 1.0,
+            constant: 0.0)
+        
+        self.view.addSubview(centerLabel)
+        
+        self.view.addConstraint(centerXConstraint)
+        self.view.addConstraint(centerYConstraint)
+        
+        let updateButton = UIButton()
+        updateButton.backgroundColor = UIColor.redColor()
+        updateButton.setTitle("Update", forState: UIControlState.Normal)
+        updateButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.view.addSubview(updateButton)
+        
+        var views:[NSObject: AnyObject] = ["updateButton": updateButton]
+        
+        centerXConstraint = NSLayoutConstraint(item: updateButton,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: .CenterX,
+            multiplier: 1.0,
+            constant: 0.0)
+        
+        var horzConstraints = NSLayoutConstraint.constraintsWithVisualFormat("[updateButton(100)]",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: views)
+        var vertConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[updateButton(50)]-10-|",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: views)
+        
+        updateButton.addConstraints(horzConstraints)
+        self.view.addConstraint(centerXConstraint)
+        self.view.addConstraints(vertConstraints)
+        
+        for theConstraint in self.view.constraints() {
+            // ...
+        }
+        
+        let lineView = UIView()
+        lineView.backgroundColor = UIColor.redColor()
+        lineView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.view.addSubview(lineView)
+        
+        views = ["centerLabel": centerLabel, "lineView": lineView]
+        
+        horzConstraints = NSLayoutConstraint.constraintsWithVisualFormat("[lineView(==centerLabel)]",
+            options: NSLayoutFormatOptions.allZeros,
+            metrics: nil,
+            views: views)
+        
+        let metrics = ["margin":NSNumber(integer: 5), "lineViewHeight":NSNumber(integer: 4)]
+        vertConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[centerLabel]-margin-[lineView(lineViewHeight)]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: metrics,
+            views: views)
+        
+        self.view.addConstraints(horzConstraints)
+        self.view.addConstraints(vertConstraints)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
